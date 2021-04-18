e6b9cf61a53b5c8c8fbce370648e4bbec032e32c
alessandro <alesssandrovasca1979@gmail.com>
2021-04-18 12:16:55 +0200

    prima modifica

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//prova_git
+int main(int argc, char *argv[]) {
+	return 0;
+}
